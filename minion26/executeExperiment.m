d4632f4ecc850f5774ed55ee187b7ee232a25235
James Ferlez <jferlez@gmail.com>
2022-09-01 07:19:26 -0700

    Decrease number of time steps to allow for compute time/cost

--- minion26/executeExperiment.m
+++ minion26/executeExperiment.m
@@ -70,7 +70,7 @@ feedbackMap = [0]; % feedback map, y[k]
 
 ncs = NNCS(NN_Controller, Plant, feedbackMap); % the neural network control system
 
-args.numSteps = 10;
+args.numSteps = 5;
 args.numCores = 1;
 args.ref_input = [];
 args.reachMethod = 'approx-star';
